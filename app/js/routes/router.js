3f2627959e2a03a8943472b89d2b8c7494ba9f4d
Cameron Woodmansee <cameron29w@gmail.com>
2013-04-30 14:01:53 -0400

    Fixed property names in template

--- app/js/routes/router.js
+++ app/js/routes/router.js
@@ -1,46 +1,46 @@
 App.Router.map(function() {
-	this.resource('search', function() {
-		this.route('filter');
-	});
+  this.resource('search', function() {
+    this.route('filter');
+  });
 });
 
 App.Router.reopen({
-	//location: 'history'
+  //location: 'history'
 });
 
 App.IndexRoute = Em.Route.extend({
-	redirect: function() {
-		this.transitionTo('search');
-	}
+  redirect: function() {
+    this.transitionTo('search');
+  }
 });
 
 App.SearchRoute = Em.Route.extend({
-	model: function() {
-		return json;
-	},
-	events: {
-		selectSearchItem: function(filterName) {
-			this.controllerFor('searchFilter').get('filters').push(filterName);
-			this.transitionTo('search.filter');
-		}
-	}
+  model: function() {
+    return json;
+  },
+  events: {
+    selectSearchItem: function(filterName) {
+      this.controllerFor('searchFilter').get('filters').push(filterName);
+      this.transitionTo('search.filter');
+    }
+  }
 });
 
 App.SearchFilterRoute = Em.Route.extend({
-	setupController: function(controller) {
-		this._super(controller);
+  setupController: function(controller) {
+    this._super(controller);
 
-		App.Search.filter(controller.get('filters')).then(function(data) {
-			var people = controller.get('people');
-			data.results.people.map(function(person) {
-				people.push(App.Person.create(person));
-			});
-			controller.set('projects', data.results.projects);
-		});
-	},
-	events: {
-		selectSearchItem: function(term) {
-			this.controller.get('filters').add(term);
-		}
-	}
+    App.Search.filter(controller.get('filters')).then(function(data) {
+      var people = controller.get('people');
+      data.results.people.map(function(person) {
+        people.push(App.Person.merge(person));
+      });
+      controller.set('projects', data.results.projects);
+    });
+  },
+  events: {
+    selectSearchItem: function(term) {
+      this.controller.get('filters').add(term);
+    }
+  }
 });
