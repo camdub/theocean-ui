8c3192a9dd7953ddb3d7197bcec47ba52b608d7d
Cameron Woodmansee <cameron29w@gmail.com>
2014-04-23 15:18:34 -0400

    Update read me, add new user default and client default

--- app/components/profile-img.js
+++ app/components/profile-img.js
@@ -1,10 +1,22 @@
 // TODO: Resource interpreted as Image but transferred with MIME type text/html
 export default Em.Component.extend({
-  didInsertElement: function() {
+
+  classNameBindings: ['size', 'type'],
+  type: null,
+
+  defaultImg: function() {
+    var path = '/assets/images/';
+    var type = this.get('type');
+    this.$().html('<img src="'+ path + type+ '.svg" />');
+  },
+
+  lazyLoad: function() {
     var image = this.get('image');
+    this.defaultImg();
+
     if(image !== '') {
       var img = new Image();
-      var selector = this.$('.profile-img img');
+      var selector = this.$('img');
       img.onload = function() {
         // make sure this component is still in DOM
         if(selector)
@@ -12,5 +24,6 @@ export default Em.Component.extend({
       };
       img.src = image;
     }
-  }
+  }.on('didInsertElement').observes('image')
+
 });
