f54a551ce6f0014311b46de44441c601ae11bbfd
Cameron Woodmansee <cameron29w@gmail.com>
2014-04-29 23:49:59 -0400

    first pass at grid results

--- app/controllers/people.js
+++ app/controllers/people.js
@@ -1,7 +1,7 @@
 import PaginationMixin from 'appkit/mixins/pagination';
 
 export default Em.ArrayController.extend(PaginationMixin, {
-  perPage: 5,
+  perPage: 10,
   page: 1,
   modelType: 'person'
 });
