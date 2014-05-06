d4d9db6f3f070b7b0c236bb6f0c50b850a49f395
Cameron Woodmansee <cameron29w@gmail.com>
2014-05-06 15:46:32 -0400

    More Bower

--- deploy.sh
+++ deploy.sh
@@ -115,7 +115,7 @@ fi
 if [ -e "$DEPLOYMENT_SOURCE/bower.json" ]; then
   eval $NPM_CMD install bower
   exitWithMessageOnError "installing bower failed"
-  ./node_modules/bower/bin/bower install -fp
+  ./node_modules/.bin/bower install -fp
   exitWithMessageOnError "bower failed"
 fi
 
